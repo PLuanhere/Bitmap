90b376562cc01034692aa3a8f0e575768c29bbd0
luanphan <24675951.luan@student.iuh.edu.vn>
2025-10-27 18:48:52 +0700

    Load 10 images

---
+++ app/src/main/java/com/example/bitmaplearn/ImageAdapter.java
@@ -0,0 +1,81 @@
+package com.example.bitmaplearn;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private List<String> imageUrls;
+    private Context context;
+
+    public ImageAdapter(Context context, List<String> imageUrls) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageViewItem);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ImageViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        String url = imageUrls.get(position);
+        new Thread(() -> {
+            Bitmap bitmap = getBitmapFromURL(url);
+            holder.imageView.post(() -> holder.imageView.setImageBitmap(bitmap));
+        }).start();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(imageUrls != null){
+            return imageUrls.size();
+        }
+        return 0;
+    }
+
+    public Bitmap getBitmapFromURL(String src) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(src)
+                .build();
+        try(Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+            InputStream input = response.body().byteStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            return BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_background);
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/bitmaplearn/MainActivity.java
+++ app/src/main/java/com/example/bitmaplearn/MainActivity.java
@@ -1,25 +1,22 @@
 package com.example.bitmaplearn;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private RecyclerView recyclerView;
+    private ImageAdapter adapter;
+    private List<String> imageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +24,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
-        imageView.setImageBitmap(getBitmapFromURL("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/1.jpg"));
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/1.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/2.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/3.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/4.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/5.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/6.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/7.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/8.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/9.jpg");
+        imageUrls.add("https://vltrfdvwnuioloivsfvh.supabase.co/storage/v1/object/public/Image/Images/10.jpg");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ImageAdapter(this, imageUrls);
+        recyclerView.setAdapter(adapter);
 
     }
 
-    public static Bitmap getBitmapFromURL(String src) {
-        try {
-            URL url = new URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+
 }
